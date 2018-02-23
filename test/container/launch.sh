be8c2506bf9a93571431557a2de2746632deefd1
Gerard Gine <ggine@swiftstack.com>
2018-02-23 15:18:28 -0800

    Debugging...

--- test/container/launch.sh
+++ test/container/launch.sh
@@ -3,40 +3,53 @@
 set -e
 set -x
 
-# Install tools needed above what's in a minimal base box
-yum -y install sudo wget git
+# Yum install. Trying to do as much as possible at once.
+## Install Golang
+yum -y --disableexcludes=all install gcc
+yum -y install \
+    ## Install Golang
+    wget \
+    ## Install Python pip
+    epel-release \
+    ## Setup ProxyFS build environment
+    sudo json-c-devel fuse \
+    ## Setup Samba
+    git \
+    gcc-c++-4.8.5-16.el7_4.1 \
+    python-devel-2.7.5-58.el7 \
+    gnutls-devel-3.3.26-9.el7 \
+    libacl-devel-2.2.51-12.el7 \
+    openldap-devel-2.4.44-5.el7 \
+    samba-4.6.2-12.el7_4 \
+    samba-client-4.6.2-12.el7_4 \
+    cifs-utils-6.2-10.el7 \
+    ## Install liberasurecode
+    http://www.rpmfind.net/linux/fedora/linux/releases/25/Everything/x86_64/os/Packages/l/liberasurecode-1.1.1-1.fc25.x86_64.rpm \
+    http://www.rpmfind.net/linux/fedora/linux/releases/25/Everything/x86_64/os/Packages/l/liberasurecode-devel-1.1.1-1.fc25.x86_64.rpm
+## Install Python pip
+yum -y install python-pip
+
+# Pip install
+pip install --upgrade pip
+pip install \
+    ## Setup ProxyFS build environment
+    requests \
+    ## Install Python tox
+    tox
 
 # Install Golang
-
-yum -y --disableexcludes=all install gcc
 cd /tmp
 wget -q https://storage.googleapis.com/golang/go1.8.3.linux-amd64.tar.gz
 tar -C /usr/local -xf go1.8.3.linux-amd64.tar.gz
 rm -rf go1.8.3.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
-# Install Python pip
-yum -y install epel-release
-yum -y install python-pip
-pip install --upgrade pip
-
 # Setup ProxyFS build environment
-pip install requests
-yum -y install json-c-devel
-yum -y install fuse
 export GOPATH=/gopathroot
 export PATH=$PATH:$GOPATH/bin
 echo "user_allow_other" >> /etc/fuse.conf
 
 # Setup Samba
-yum -y install gcc-c++-4.8.5-16.el7_4.1 \
-               python-devel-2.7.5-58.el7 \
-               gnutls-devel-3.3.26-9.el7 \
-               libacl-devel-2.2.51-12.el7 \
-               openldap-devel-2.4.44-5.el7 \
-               samba-4.6.2-12.el7_4 \
-               samba-client-4.6.2-12.el7_4 \
-               cifs-utils-6.2-10.el7
 cd $GOPATH/src/github.com/swiftstack/ProxyFS/vfs
 git clone -b samba-4.6.12 --single-branch --depth 1 https://github.com/samba-team/samba.git samba4-6-12-centos
 ln -s samba4-6-12-centos samba
@@ -45,13 +58,6 @@ cd samba
 make clean
 make GEN_NDR_TABLES
 export SAMBA_SOURCE=$GOPATH/src/github.com/swiftstack/ProxyFS/vfs/samba
-
-# Install Python tox
-pip install tox
-
-# Install liberasurecode
-yum -y install http://www.rpmfind.net/linux/fedora/linux/releases/25/Everything/x86_64/os/Packages/l/liberasurecode-1.1.1-1.fc25.x86_64.rpm
-yum -y install http://www.rpmfind.net/linux/fedora/linux/releases/25/Everything/x86_64/os/Packages/l/liberasurecode-devel-1.1.1-1.fc25.x86_64.rpm
 
 # Build ProxyFS and run tests
 cd $GOPATH/src/github.com/swiftstack/ProxyFS
--- travis_emulator.sh
+++ travis_emulator.sh
@@ -3,7 +3,5 @@
 set -e
 set -x
 
-#docker build -t proxyfs test/container
-#cidfile=$(mktemp -u)
-#docker run -it -v `pwd`:/gopathroot/src/github.com/swiftstack/ProxyFS --cidfile "${cidfile}" proxyfs bash -c "/gopathroot/src/github.com/swiftstack/ProxyFS/test/container/launch.sh"
+docker build -t proxyfs test/container
 docker run -it -v `pwd`:/gopathroot/src/github.com/swiftstack/ProxyFS proxyfs bash -c "/gopathroot/src/github.com/swiftstack/ProxyFS/test/container/launch.sh"
