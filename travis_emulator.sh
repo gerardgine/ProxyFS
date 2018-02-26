35f6436bd1d0c0886638ab05dc81ba5262e2aae6
Gerard Gine <ggine@swiftstack.com>
2018-02-26 10:15:00 -0800

    Debugging...

--- travis_emulator.sh
+++ travis_emulator.sh
@@ -6,3 +6,4 @@ set -x
 rm -rf vfs/samba*
 docker build -t proxyfs test/container
 docker run -it -v `pwd`:/gopathroot/src/github.com/swiftstack/ProxyFS proxyfs bash -c "/gopathroot/src/github.com/swiftstack/ProxyFS/test/container/launch.sh"
+
